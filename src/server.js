eda6a2d035b746315ead3971e96729cd7ea69963
W. Fernando <wellysonf@gmail.com>
2023-07-11 19:49:37 -0300

    correção do server para o deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import { app } from "./app.js";
 
 app.listen({
-    port: 3333
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
 }).then(()=>{
     console.log('Server iniciado.');
 })
